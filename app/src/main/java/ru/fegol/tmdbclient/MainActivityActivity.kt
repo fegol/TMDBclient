97f1a29042bf3377eace12cb77b8f1cd24bf5bc5
Мамонов Андрей Юрьевич <AYuMamonov.SBT@sberbank.ru>
2017-11-28 18:51:19 +0300

    initial commit

---
+++ app/src/main/java/ru/fegol/tmdbclient/MainActivityActivity.kt
@@ -0,0 +1,12 @@
+package ru.fegol.tmdbclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
